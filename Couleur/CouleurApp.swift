931c217effd4e20f94d8aaf6a99386ffd94c630b
Valentina <balendi.art@gmail.com>
2023-03-14 11:28:31 +0100

    Initial Commit

---
+++ Couleur/CouleurApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouleurApp.swift
+//  Couleur
+//
+//  Created by Apprenant 18 on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CouleurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
